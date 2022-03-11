5deaa732b230e22c5d81ef97a81052210d5023f6
EugenAchtzehn <eugene1826@gmail.com>
2022-03-11 22:03:39 +0800

    Rebuild js using airbnb-eslint style

--- js/all.js
+++ js/all.js
@@ -1,57 +1,51 @@
-// TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-var url="https://hexschool.github.io/js-filter-data/data.json"
-var data;
+// TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 > done
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
 
-axios.get(url)
- .then(function(res){
-  data=res.data.filter(a=>a.作物名稱)
-  // TODO: 之後拆成 renderData 函式
-  var str=""
-  data.forEach((b,index)=>{
-    // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
-    str+=content
-  })
-  table.innerHTML=str
-})
+// 選取 HTML 元素
+const table = document.querySelector('.table-content');
+const filter = document.querySelector('.filter');
 
-var table=document.querySelector(".table-content")
-var showData=[]
+// 全域使用的變數宣告
+let data;
+let showData = [];
+let category = '';
 
-var category=""
-var filter=document.querySelector(".filter")
+// 全域使用的函式宣告
+function renderData(ary) {
+  let str = '';
+  ary.forEach((plantObj) => {
+    const tableRow = `
+    <tr>
+      <td> ${plantObj.作物名稱} </td>
+      <td> ${plantObj.市場名稱} </td>
+      <td> ${plantObj.上價} </td>
+      <td> ${plantObj.中價} </td>
+      <td> ${plantObj.下價} </td>
+      <td> ${plantObj.平均價} </td>
+      <td> ${plantObj.交易量} </td>
+    </tr>`;
+    str += tableRow;
+  });
+  table.innerHTML = str;
+}
 
-filter.addEventListener("click",filterCategory)
+// 由 CDN 引入的 axios，要加上 window 表示其為掛載在全域下的物件
+window.axios.get(url).then((res) => {
+  // 排除沒有'作物名稱'屬性的物件，組成陣列 data
+  data = res.data.filter((plantObj) => plantObj.作物名稱);
+  // TODO: 之後拆成 renderData 函式 > done
+  renderData(data);
+});
 
-function filterCategory(e){
-  if(e.target.nodeName=="BUTTON"){
-    category=e.target.dataset.category
-    showData=data.filter((i)=>{
-      return i.種類代碼==category
-    })
-    // TODO: 之後拆成 renderData 函式
-    var str=""
-    showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
-      str+=content
-    })
-    table.innerHTML=str
-  }else{
-    return;
+function filterCategory(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    // 蔬果的 category 是 N04, 水果是 N05
+    category = e.target.dataset.category;
+    showData = data.filter((plantObj) => plantObj.種類代碼 === category);
+    // TODO: 之後拆成 renderData 函式 > done
+    renderData(showData);
   }
 }
 
+// 針對 filter 區的監聽函式
+filter.addEventListener('click', filterCategory);
